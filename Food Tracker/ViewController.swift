2eb757c65a4cb5c6f2abae63f543e01c7fb39a1c
maurerpower12 <josephmau12@gmail.com>
2017-12-12 20:33:01 -0800

    Project files.

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Joseph Maurer on 8/14/17.
+//  Copyright © 2017 Joseph Maurer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
