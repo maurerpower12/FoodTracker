dd684884639622035ba93ca493c0374b1077d1ca
maurerpower12 <josephmau12@gmail.com>
2018-02-09 21:51:16 -0800

    Added data model and unit tests

---
+++ Meal.swift
@@ -0,0 +1,43 @@
+//
+//  Meal.swift
+//  Food Tracker
+//
+//  Created by Joseph Maurer on 2/9/18.
+//  Copyright © 2018 Joseph Maurer. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //Mark: Properties
+    
+    var name: String
+    
+    var photo: UIImage?
+    
+    var rating: Int
+    
+    // MarK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no 
+        // name or if the rating is negative.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
