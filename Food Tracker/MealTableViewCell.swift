3d6f619f1fec7628b270c8cd7616f5f04bfaa303
maurerpower12 <josephmau12@gmail.com>
2018-02-09 22:16:32 -0800

    Added List view to main story board.
    Started custom cell

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Joseph Maurer on 2/9/18.
+//  Copyright © 2018 Joseph Maurer. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
