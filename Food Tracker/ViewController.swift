677ec983d051b2d7fbeed54c6395d010fd5a9b08
maurerpower12 <josephmau12@gmail.com>
2018-02-12 21:54:42 -0800

    Cancel button functionality

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -67,6 +67,10 @@ class ViewController: UIViewController, UITextFieldDelegate,
     
     //MARK: Navigation
     
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
