e832929281c9b3e96127e3464baf033e52133311
maurerpower12 <josephmau12@gmail.com>
2018-02-14 21:52:49 -0800

    conditional cancel button functionality

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -68,7 +68,18 @@ class ViewController: UIViewController, UITextFieldDelegate,
     //MARK: Navigation
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
